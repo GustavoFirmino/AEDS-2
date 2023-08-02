4385de867eed36945b522ed6f287b7077ed97a96
Gustavo Pessoa Firmino Duarte <103456460+GustavoFirmino@users.noreply.github.com>
2023-08-02 15:17:26 -0300

    Add files via upload

---
+++ Atividades Verde/Palíndromo/palindromo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class palindromo{
+    public static void main(){
+        Scanner sc = new Scanner(System.in);
+        String palavra = sc.nextLine();
+        int tamanho = 0;
+        
+        while(palavra != "FIM"){
+            
+        }
+        
+    }
+}
