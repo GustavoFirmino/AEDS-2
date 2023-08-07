5e71edc93d95f4a53b17aa6cff9de4702d5fe526
Gustavo Pessoa Firmino Duarte <103456460+GustavoFirmino@users.noreply.github.com>
2023-08-06 23:56:56 -0300

    Add files via upload

--- Atividades Verde/Palíndromo/palindromo.java
+++ Atividades Verde/Palíndromo/palindromo.java
@@ -1,14 +1,25 @@
 import java.util.*;
 
 class palindromo{
-    public static void main(){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String palavra = sc.nextLine();
-        int tamanho = 0;
-        
+        int tamanho = palavra.length();
+        int cont = 0;
+
         while(palavra != "FIM"){
-            
+            for(int i=0; i>=tamanho-1; i++,tamanho--){
+                if(palavra.charAt(i)==palavra.charAt(tamanho-1)){
+                    cont++;
+                }
+            }
+            if(cont == tamanho/2){
+                System.out.println("SIM");
+            }
+            else if(cont != tamanho/2){
+                System.out.println("NAO");
+            }
         }
-        
+        sc.close();
     }
 }
