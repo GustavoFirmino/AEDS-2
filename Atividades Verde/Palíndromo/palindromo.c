b061c121e10b4ccf0df2ccbbe2fc3c8fbc8d64f7
Gustavo Pessoa Firmino Duarte <103456460+GustavoFirmino@users.noreply.github.com>
2023-08-09 08:33:40 -0300

    Add files via upload

--- Atividades Verde/Palíndromo/palindromo.c
+++ Atividades Verde/Palíndromo/palindromo.c
@@ -2,26 +2,14 @@
 #include <string.h>
 
 int main() {
-    char palavra[100]; // Defina um tamanho máximo para a palavra
-    while (fgets(palavra, sizeof(palavra), stdin)) {
-        palavra[strcspn(palavra, "\n")] = '\0'; // Remove a quebra de linha da entrada
+    char palavra[100]; 
+    char contrario[100];
 
-        if (strcmp(palavra, "FIM") == 0) {
-            break;
-        }
-
-        char contrario[100] = "";
-        int tam = strlen(palavra);
-
-        for (int i = tam - 1; i >= 0; i--) {
-            strncat(contrario, &palavra[i], 1);
-        }
-
-        if (strcmp(palavra, contrario) == 0) {
-            printf("SIM\n");
-        } else {
-            printf("NAO\n");
-        }
-    }
-    return 0;
+scanf("%s", palavra);
+while(palavra != "FIM"){
+for(int i=(strlen(palavra)-1,j=0); i>0;i-,j++-){
+contrario[j] += palavra[i];
+}
+if(contrario  
+}
 }
